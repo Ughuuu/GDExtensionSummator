b1ad28bec3004bfecbbcb2cd816f863e211935a9
Dragos Daian <daian.dragos@yahoo.com>
2024-04-01 20:08:29 +0200

    add action for build

--- create_compat_layer.sh
+++ create_compat_layer.sh
@@ -1,5 +1,4 @@
 cd ../godot
-chmod +x ../GDExtensionSummator/godot-cpp/compat_generator.py
 python ../GDExtensionSummator/godot-cpp/compat_generator.py
 cp output_header_mapping.json ../GDExtensionSummator/godot-cpp/output_header_mapping.json
 cd ../GDExtensionSummator
